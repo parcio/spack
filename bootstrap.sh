45fc8f79f73fbdf1a466da98bf18b901f909e791
Michael Kuhn <michael.kuhn@ovgu.de>
2023-11-07 21:44:25 +0100

    bootstrap: Apply more PRs

--- bootstrap.sh
+++ bootstrap.sh
@@ -147,6 +147,9 @@ then
 	bootstrap_apply_pr 40612
 	# julia
 	bootstrap_apply_pr 40911
+	bootstrap_apply_pr 40938
+	# py-torch
+	bootstrap_apply_pr 40893
 
 	rm --force --recursive "${HOME}/.spack"
 
